7973f8f1bd201820b2791b33591d50b8038740ae
John <alice613118@gmail.com>
2024-11-19 22:43:33 +0800

    移除不必要的 SVG 檔案與全域 CSS，並更新首頁結構以包含導航連結

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +11,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="zh-Hant">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
+      <body>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-// import Image from "next/image";
+import React from 'react';
+import Link from 'next/link';
+import '../../styles/global/global.css';
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      <nav>
+        <Link href="/book">
+          Book
+        </Link>
+      </nav>
+      <h1>Home Page</h1>
     </div>
   );
 }
