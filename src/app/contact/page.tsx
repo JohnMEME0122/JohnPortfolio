0e76c2e0742de66dd43558c727c7cb8df2b15a8c
John <alice613118@gmail.com>
2024-11-20 23:54:26 +0800

    新增聯絡頁面與作品集頁面，並調整導航欄樣式

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import '@styles/global/global.css';
+import NavBar from '@components/NavBar';
+
+export default function Contact() {
+  return (
+    <div>
+      <NavBar />
+      <h1>contact Page</h1>
+    </div>
+  );
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import '@styles/global/global.css';
+import NavBar from '@components/NavBar';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Portfolio Page</h1>
+    </div>
+  );
+}
